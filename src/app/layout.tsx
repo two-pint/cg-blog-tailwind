6379511089fa9289c08fbdde053b89192dd46070
chubbymaus <cgarr73@gmail.com>
2024-09-02 20:50:32 -0600

    remove xml feed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,6 @@ export const metadata: Metadata = {
   },
   description:
     'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
-  alternates: {
-    types: {
-      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
-    },
-  },
 }
 
 export default function RootLayout({
